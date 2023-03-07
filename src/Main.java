440c1a077d7d03ec1cdff6dca5056f59d9aadf7a
DejanST89 <Dejan.Stancevic@hotmail.com>
2023-03-07 20:20:54 +0100

    Update data

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.print("Unesite snagu automobila u Kw:");//Program za izracunavanje konjske snage automobila
 
     int snagaKw = s.nextInt();
-    double snagaKS = snagaKw * 1.90;
+    double snagaKS = snagaKw * 2.90;
 
         System.out.println("snaga automobila u konjskim snagama"+snagaKS);
        System.out.println("Program je zavrsio svoj rad");
