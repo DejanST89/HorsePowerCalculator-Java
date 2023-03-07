98e907cfc451c770487c212efccf8bbba91f2b22
DejanST89 <Dejan.Stancevic@hotmail.com>
2023-03-07 19:52:47 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+
+
+   Scanner s = new Scanner(System.in);
+        System.out.print("Unesite snagu automobila u Kw:");//Program za izracunavanje konjske snage automobila
+
+    int snagaKw = s.nextInt();
+    double snagaKS = snagaKw * 1.36;
+
+        System.out.println("snaga automobila u konjskim snagama"+snagaKS);
+       System.out.println("Program je zavrsio svoj rad");
+
+
+    }
+}
